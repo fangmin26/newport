cce19d0194d546ab9712ed63b8ee6c3c7c32f31a
fangmin26 <fangmin_@naver.com>
2021-07-13 22:47:54 +0900

    713

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+//https://suprecontent.com/creative
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import { Main } from "Main";
+import Main from "./component/Main";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={Main}></Route>
+      <Route exact path="/" component={Main} />
     </BrowserRouter>
   );
 };
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Layout from "../basis/Layout";
+//import Layout from "../basis/Layout";
 
 const Main = () => {
-  return <Layout>this is main</Layout>;
+  return <div>this is main</div>;
 };
 
 export default Main;
